a4b9a687924253f68756252d5bb2a4f9dd74122f
John S. Tonello <21206201+jtonello@users.noreply.github.com>
2021-04-26 19:54:38 -0400

    Create default.rb

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default['audit']['reporter'] = ['chef-automate', 'cli']
+default['audit']['fetcher'] = 'chef-automate'
+default['audit']['profiles']['User jtonello exists'] = {
+  'name': 'User jtonello exists',
+  'compliance': 'admin/create-user',
+}
+default['audit']['compliance_phase'] = true
